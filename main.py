2bb6916251ff9bc6d9739e41269fc0315f149bc1
Yvann99 <landureyvann@gmail.com>
2026-02-13 10:45:17 +0100

    Exercice normalisation

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-def main():
-    print("Hello from datascience!")
+#Bibliothèques
+import yfinance as yf
+import numpy as np
+import pandas as pd
+#Data
+tickers = {"GLE.PA","^FCHI"}
+data_set = yf.download(tickers, period = "3Y")["Close"]
+data_set_clean = data_set.dropna()
+#calcul des log returns
+log_returns = np.log(data_set_clean/data_set_clean.shift(1))
+log_returns.columns = [f"Log_Ret_{col}" for col in log_returns.columns]
 
-
-if __name__ == "__main__":
-    main()
-
-
-import pandas
+resultat = pd.concat([data_set_clean, log_returns], axis=1).dropna()
+print(resultat.head())
